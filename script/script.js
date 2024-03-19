c7bec61435e63d18864ff18dfd76f53da8729d89
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-03-19 15:34:33 -0500

    created workout class

--- script/script.js
+++ script/script.js
@@ -5,4 +5,30 @@ class newWorkout {
         this.excerciseName = excerciseName;
         this.duration = duration;
     }
+
+    displayWorkout(){
+        return `Excercise Type: ${this.excerciseType} | Excercise Name: ${this.excerciseName} | Duration ${this.duration}`;
+    }
+}
+
+const workout = new newWorkout("Cardio", "Treadmill", 10);
+workout.displayWorkout();
+
+
+
+let workoutLog = [];
+
+let logWorkout = {
+    excerciseType: "Cardio",
+    excerciseName: "Treadmill",
+    duration: 10
+};
+
+
+
+
+function workoutSummary() {
+
+return workoutLog.length;
+
 }
