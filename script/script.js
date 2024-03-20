8b216df5b023e203fd0b2aed1718d43443b8a514
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-03-20 13:13:47 -0500

    created an extension for newWorkout class

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-
+// class for new workout
 class newWorkout {
     constructor(excerciseType, excerciseName, duration) {
         this.excerciseType = excerciseType;
@@ -11,18 +11,44 @@ class newWorkout {
     }
 }
 
-const workout = new newWorkout("Cardio", "Treadmill", 10);
-workout.displayWorkout();
+
+class moreWorkout extends newWorkout {
+    constructor(excerciseType, excerciseName, duration, sets, reps){
+        super(excerciseType, excerciseName, duration);
+        this.sets = sets;
+        this.reps = reps
+    }
+    displayWorkout(){
+        return `${super.displayWorkout()}, Sets: ${this.sets}, | Reps: ${this.reps}`;
+    }
+}
 
 
 
+function addWorkout() {
+    const excerciseName = document.getElementById("excercise-name").value;
+    const excerciseType = document.getElementById("excercise-type").value;
+    const duration = parseFloat(document.getElementById("duration").value);
+    const set = parseInt(document.getElementById("set").value);
+    const reps = parseInt(document.getElementById("reps").value);
+
+
+    const workout = new newWorkout("Cardio", "Treadmill", 10);
+    displayLog();
+}
+
+
+
+
+// array for past workout log
 let workoutLog = [];
 
-let logWorkout = {
-    excerciseType: "Cardio",
-    excerciseName: "Treadmill",
-    duration: 10
-};
+let logWorkout = [
+    { excerciseType: "Cardio", excerciseName: "Treadmill",  duration: 10},
+    { excerciseType: "Upper Body", excerciseName: "Bench Press",  duration: 20, sets: 3, reps: 8},
+    { excerciseType: "Lower Body", excerciseName: "Squats",  duration: 10, sets: 3, reps: 8},
+
+];
 
  
 
